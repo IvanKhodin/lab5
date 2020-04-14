d98714546a473304194f1672b67484d29ce4a638
IvanKhodin <62280809+IvanKhodin@users.noreply.github.com>
2020-04-14 10:07:28 +0300

    Add files via upload

---
+++ MMM5.cpp
@@ -0,0 +1,93 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <stdlib.h> 
+#include <cstdlib>
+#include <ctime>
+#include <string>
+#include <math.h>
+#include <fstream>
+
+using namespace std;
+
+void l()
+{
+    cout << endl;
+}
+                                                                                                           
+double f(double z)
+{
+    return sin(z*z);                                                                                   
+}
+                                                                                                           
+                                                                                                            //формула Симпсона
+double S(int n, double a, double b) 
+{
+    double sum = 0, h;
+    
+    h = (b - a) / n;
+    
+    for (double i = 1; i < n; i += 2)
+        sum += f(a + (i - 1) * h) + 4 * f(a + i * h) + f(a + (i + 1) * h);
+    
+    return h / 3 * sum;
+
+}
+
+double R(int n, double a, double b) 
+{
+    double s = 0, h = (b - a) / n;
+    
+    for (int i = 1; i < n; i++)
+    {
+        s += f(a + i * h);
+    }
+    
+    return h * (f(a) / 2 + s + f(b) / 2);
+
+}
+
+int main() {
+
+    cout << "Khodin Ivan, 9 variant" << endl << endl;
+
+    const double PI = 3.141592653589793;
+    
+    ofstream f1;
+    f1.open("ans1.dat");
+    
+    ofstream f2;
+    f2.open("ans2.dat");
+    
+    double a = 0, b = PI/4, e = 0.0001;
+    
+    int n = 20;
+    
+    while (abs((S(n, a, b) - S(n / 2, a, b)) / S(n, a, b)) >= e) {
+        n *= 2;
+    }
+    
+    cout << n << " " << S(n, a, b) << " " << abs((S(n, a, b) - S(n / 2, a, b)) / S(n, a, b)) << endl;
+    
+    f1 << S(n, a, b) << " " << abs((S(n, a, b) - S(n / 2, a, b)) / S(n, a, b)) << endl;
+
+
+
+    n = 20;
+    
+    while (abs((R(n, a, b) - R(n / 2, a, b)) / R(n, a, b)) >= e) {
+        n *= 2;
+    }
+    
+    cout << n << " " << R(n, a, b) << " " << abs((R(n, a, b) - R(n / 2, a, b)) / R(n, a, b)) << endl;
+    
+    f2 << R(n, a, b) << " " << abs((R(n, a, b) - R(n / 2, a, b)) / R(n, a, b)) << endl;
+
+    l();
+
+    f1.close();
+
+    f2.close();
+
+    return 0;
+
+}
---
+++ MMM5_py.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+def f(x):
+    math.sin(x*x)
+
+def Simp(n, a, b):
+    s=0
+    h=(b-a)/n
+    for i in range(1, n, 2):
+        s+= f(a + (i-1)*h) + 4* f(a+  i*h) + f(a+ (i+1)*h)
+    return h/3*s
+
+def Rect(n, a, b):
+    s=0
+    h= (b-a)/n
+    for i in range(1, n):
+        s+= f(a + i*h)
+    return h* (f(a)/2 + s + f(b)/2)
+
+a=0
+b=1
+e=0.0001 
+n=20
+while abs((Simp(n,a,b)- Simp(n//2, a, b))/Simp(n, a, b)) >=e:
+    n*=2
+print(n, Simp(n,a,b), abs((Simp(n,a,b)- Simp(n//2, a, b))/Simp(n, a, b)))
+
+n=20
+while abs((Rect(n,a,b)- Rect(n//2, a, b))/Rect(n, a, b)) >=e:
+    n*=2
+print(n, Rect(n,a,b), abs((Rect(n,a,b)- Rect(n//2, a, b))/Rect(n, a, b)))
